a7d2e8939ed01302492051e721a881dc75efa686
Ozyugoo <stellaozy22@gmail.com>
2024-03-04 14:12:32 +0100

    File: add data file chore: add data for use as API

---
+++ src/components/data/data.js
@@ -0,0 +1,19 @@
+const reviewData = [
+    {
+        id: 1,
+        rating: 7,
+        text: 'Good.'
+    },
+    {
+        id: 2,
+        rating: 8,
+        text: 'Better'
+    },
+    {
+        id: 3,
+        rating: 9,
+        text: 'Best.'
+    }
+]
+
+export default reviewData
