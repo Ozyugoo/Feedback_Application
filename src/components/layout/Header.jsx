aadffcfb561f486efdb863b392d1b0ed99eb44ec
Ozyugoo <stellaozy22@gmail.com>
2024-03-04 14:13:46 +0100

    Chore: add header component

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,14 @@
+
+
+// eslint-disable-next-line react/prop-types
+function Header({text}) {
+  return (
+    <>
+      <header>
+        <h1>{text}</h1>
+      </header>
+    </>
+  )
+}
+
+export default Header
